9d935ac015bc9f501146e7715257f88d3b318a89
Steve Ripberger <steve.ripberger@zipscene.com>
2017-08-16 10:15:41 -0400

    Finish splitting sync and async paths

--- test/unit/run-utils.js
+++ test/unit/run-utils.js
@@ -2,9 +2,10 @@ const runUtils = require('../../lib/run-utils');
 const sinon = require('sinon');
 const Runner = require('../../lib/runner');
 const settingsUtils = require('../../lib/settings-utils');
+const TestChromosome = require('../lib/test-chromosome');
 const TestIndividual = require('../lib/test-individual');
 
-describe.skip('runUtils', function() {
+describe('runUtils', function() {
 	let sandbox;
 
 	beforeEach(function() {
@@ -16,25 +17,88 @@ describe.skip('runUtils', function() {
 	});
 
 	describe('::run', function() {
-		it('runs a genetic algorithm with the provided settings', function() {
+		let settings, processedSettings, best;
+
+		beforeEach(function() {
+			settings = { foo: 'bar' };
+			processedSettings = { baz: 'qux' };
+			best = new TestChromosome('best');
+
+			sandbox.stub(settingsUtils, 'process').returns(processedSettings);
+			sandbox.stub(runUtils, 'runSync').returns(best);
+			sandbox.stub(runUtils, 'runAsync').resolves(best);
+		});
+
+		it('processes provided settings', function() {
+			runUtils.run(settings);
+
+			expect(settingsUtils.process).to.be.calledOnce;
+			expect(settingsUtils.process).to.be.calledOn(settingsUtils);
+			expect(settingsUtils.process).to.be.calledWith(settings);
+		});
+
+		context('async setting is not set', function() {
+			it('returns result of ::runSync with processed settings', function() {
+				let result = runUtils.run(settings);
+
+				expect(runUtils.runSync).to.be.calledOnce;
+				expect(runUtils.runSync).to.be.calledOn(runUtils);
+				expect(runUtils.runSync).to.be.calledWith(processedSettings);
+				expect(result).to.equal(best);
+			});
+		});
+
+		context('async setting is set', function() {
+			it('resolves with result of ::runAsync with processed settings', function() {
+				processedSettings.async = {};
+
+				return runUtils.run(settings)
+					.then((result) => {
+						expect(runUtils.runAsync).to.be.calledOnce;
+						expect(runUtils.runAsync).to.be.calledOn(runUtils);
+						expect(runUtils.runAsync).to.be.calledWith(
+							processedSettings
+						);
+						expect(result).to.equal(best);
+					});
+			});
+		});
+	});
+
+	describe('::runSync', function() {
+		it('synchronously runs a genetic algorithm with provided settings', function() {
 			let settings = { foo: 'bar' };
-			let processedSettings = { baz: 'qux' };
 			let runner = new Runner();
 			let best = new TestIndividual('best');
-			sandbox.stub(settingsUtils, 'processSettings').returns(processedSettings);
-			sandbox.stub(Runner, 'create').resolves(runner);
-			sandbox.stub(runner, 'run').resolves(best);
+			sandbox.stub(Runner, 'createSync').returns(runner);
+			sandbox.stub(runner, 'runSync').returns(best);
 
-			return runUtils.run(settings)
+			let result = runUtils.runSync(settings);
+
+			expect(Runner.createSync).to.be.calledOnce;
+			expect(Runner.createSync).to.be.calledOn(Runner);
+			expect(Runner.createSync).to.be.calledWith(settings);
+			expect(runner.runSync).to.be.calledOnce;
+			expect(runner.runSync).to.be.calledOn(runner);
+			expect(result).to.equal(best.chromosome);
+		});
+	});
+
+	describe('::runAsync', function() {
+		it('asynchronously runs a genetic algorithm with the provided settings', function() {
+			let settings = { foo: 'bar' };
+			let runner = new Runner();
+			let best = new TestIndividual('best');
+			sandbox.stub(Runner, 'createAsync').resolves(runner);
+			sandbox.stub(runner, 'runAsync').resolves(best);
+
+			return runUtils.runAsync(settings)
 				.then((result) => {
-					expect(settingsUtils.processSettings).to.be.calledOnce;
-					expect(settingsUtils.processSettings).to.be.calledOn(settingsUtils);
-					expect(settingsUtils.processSettings).to.be.calledWith(settings);
-					expect(Runner.create).to.be.calledOnce;
-					expect(Runner.create).to.be.calledOn(Runner);
-					expect(Runner.create).to.be.calledWith(processedSettings);
-					expect(runner.run).to.be.calledOnce;
-					expect(runner.run).to.be.calledOn(runner);
+					expect(Runner.createAsync).to.be.calledOnce;
+					expect(Runner.createAsync).to.be.calledOn(Runner);
+					expect(Runner.createAsync).to.be.calledWith(settings);
+					expect(runner.runAsync).to.be.calledOnce;
+					expect(runner.runAsync).to.be.calledOn(runner);
 					expect(result).to.equal(best.chromosome);
 				});
 		});
