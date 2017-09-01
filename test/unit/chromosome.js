a1aea23dc13791550611482b230cf443d6e3d978
Steve Ripberger <steve.ripberger@zipscene.com>
2017-09-01 14:53:22 -0400

    Improve Chromosome::isChromosome

--- test/unit/chromosome.js
+++ test/unit/chromosome.js
@@ -3,15 +3,42 @@ const XError = require('xerror');
 
 describe('Chromosome', function() {
 	describe('::isChromosome', function() {
-		it('returns true if obj.getFitness is a function', function() {
-			expect(Chromosome.isChromosome({
-				getFitness: () => {}
-			})).to.be.true;
+		let obj;
+
+		beforeEach(function() {
+			obj = {
+				getFitness: () => {},
+				crossover: () => {},
+				mutate: () => {}
+			};
 		});
 
-		it('returns false otherwise', function() {
-			expect(Chromosome.isChromosome({})).to.be.false;
-			expect(Chromosome.isChromosome({ getFitness: 'foo' })).to.be.false;
+		it('returns false if obj.getFitness is not a function', function() {
+			obj.getFitness = {};
+
+			expect(Chromosome.isChromosome(obj)).to.be.false;
+		});
+
+		it('returns false if obj.crossover is not a function', function() {
+			obj.crossover = {};
+
+			expect(Chromosome.isChromosome(obj)).to.be.false;
+		});
+
+		it('returns false if obj.mutate is not a function', function() {
+			obj.mutate = {};
+
+			expect(Chromosome.isChromosome(obj)).to.be.false;
+		});
+
+		it('returns true otherwise', function() {
+			expect(Chromosome.isChromosome(obj)).to.be.true;
+		});
+
+		it('returns true for Chromosome instances', function() {
+			let chromosome = new Chromosome();
+
+			expect(Chromosome.isChromosome(chromosome)).to.be.true;
 		});
 	});
 
